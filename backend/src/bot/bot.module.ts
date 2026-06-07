31eab0d122068be4739fc24b306e09af5e681280
Parsa13Ah <parsafull.gamer@gmail.com>
2026-06-07 16:57:02 +0330

    Update bot.module.ts

--- backend/src/bot/bot.module.ts
+++ backend/src/bot/bot.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Bot } from '../database/entities/bot.entity';
 import { Account } from '../database/entities/account.entity';
+import { Group } from '../database/entities/group.entity';
+import { AccountGroup } from '../database/entities/account-group.entity';
+import { LinkDoniChannel } from '../database/entities/link-doni-channel.entity';
+import { Job } from '../database/entities/job.entity';
 import { BotService } from './bot.service';
 import { BotController } from './bot.controller';
 import { BotWorker } from './bot.worker';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Bot, Account])],
+  imports: [TypeOrmModule.forFeature([Bot, Account, Group, AccountGroup, LinkDoniChannel, Job])],
   controllers: [BotController],
   providers: [BotService, BotWorker],
   exports: [BotService, BotWorker],
